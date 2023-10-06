f97ab552774820d65eab97ed95e8a48b379b16f3
loic.michalon <loic.michalon@ekium.eu>
2023-10-06 10:51:27 +0200

    [EDIT] BaseKPIRTService can now have handle different data types

--- Core/Shared/Models/DB/Kernel/Interfaces/IBaseKPI.cs
+++ Core/Shared/Models/DB/Kernel/Interfaces/IBaseKPI.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
 namespace Core.Shared.Models.DB.Kernel.Interfaces;
 
-public interface IBaseKPI
+public interface IBaseKPI<TValue>
 {
-	public string GetValue();
-	public string GetKPICRID();
+	public TValue GetValue();
+	public string[] GetKPICRID();
+	public Func<TValue, TValue>[] GetComputedValue();
 }
--- Core/Shared/Services/Background/KPI/KPIRT/BaseKPIRTService.cs
+++ Core/Shared/Services/Background/KPI/KPIRT/BaseKPIRTService.cs
@@ -8,17 +8,17 @@ using Microsoft.Extensions.Logging;
 
 namespace Core.Shared.Services.Background.KPI.KPIRT;
 
-public class BaseKPIRTService<T, TDTO, TService> : BackgroundService
-	where T : class, IBaseEntity<T, TDTO>, IBaseKPI
+public class BaseKPIRTService<T, TDTO, TService, TValue> : BackgroundService
+	where T : class, IBaseEntity<T, TDTO>, IBaseKPI<TValue>
 	where TDTO : class, IDTO<T, TDTO>
 	where TService : class, IServiceBaseEntity<T, TDTO>
 {
 	private readonly IServiceScopeFactory _factory;
-	private readonly ILogger<BaseKPIRTService<T, TDTO, TService>> _logger;
+	private readonly ILogger<BaseKPIRTService<T, TDTO, TService, TValue>> _logger;
 	private readonly TimeSpan _period = TimeSpan.FromHours(24);
 	private int _executionCount;
 
-	public BaseKPIRTService(IServiceScopeFactory factory, ILogger<BaseKPIRTService<T, TDTO, TService>> logger)
+	public BaseKPIRTService(IServiceScopeFactory factory, ILogger<BaseKPIRTService<T, TDTO, TService, TValue>> logger)
 	{
 		_factory = factory;
 		_logger = logger;
@@ -43,6 +43,8 @@ public class BaseKPIRTService<T, TDTO, TService> : BackgroundService
 				await using AsyncServiceScope asyncScope = _factory.CreateAsyncScope();
 				TService tService =
 					asyncScope.ServiceProvider.GetRequiredService<TService>();
+				
+				// TODO Logic -> GetAll of T, get its KPICRIDs (create KPIC and KPIRT if necessary) and make the computations on it.
 
 				_executionCount++;
 				_logger.LogInformation("Executed BaseKPIRTService - Count: {count}", _executionCount);
