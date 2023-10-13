76230c674f233ebbf027f94f0e84c1fe7603edd4
loic.michalon <loic.michalon@ekium.eu>
2023-10-13 15:04:10 +0200

    [FEAT] MonitorService to IOTService.

---
+++ Core/Shared/Services/Background/IOTService.cs
@@ -0,0 +1,47 @@
+using Core.Entities.IOT.IOTDevices.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Shared.Services.Background;
+
+public class IOTService : BackgroundService
+{
+	private readonly IServiceScopeFactory _factory;
+	private readonly ILogger<IOTService> _logger;
+	private readonly TimeSpan _period = TimeSpan.FromSeconds(1);
+	private int _executionCount;
+
+	public IOTService(ILogger<IOTService> logger, IServiceScopeFactory factory)
+	{
+		_logger = logger;
+		_factory = factory;
+	}
+
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		using PeriodicTimer timer = new(_period);
+		while (!stoppingToken.IsCancellationRequested
+		       && await timer.WaitForNextTickAsync(stoppingToken))
+			try
+			{
+				await using AsyncServiceScope asyncScope = _factory.CreateAsyncScope();
+				IIOTDeviceService iotDeviceService =
+					asyncScope.ServiceProvider.GetRequiredService<IIOTDeviceService>();
+
+				_logger.LogInformation("IOTService running at: {time}", DateTimeOffset.Now);
+				_logger.LogInformation("Calling CheckAllConnectionsAndApplyTags");
+
+				await iotDeviceService.CheckAllConnectionsAndApplyTags();
+
+				_executionCount++;
+				_logger.LogInformation("Executed PeriodicIOTService - Count: {count}", _executionCount);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogInformation(
+					"Failed to execute PeriodicIOTService with exception message {message}. Good luck next round!",
+					ex.Message);
+			}
+	}
+}
