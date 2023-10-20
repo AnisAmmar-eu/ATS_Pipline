b4e4eec9e284b6f7b8c81fda6269e51ed1cf3f7a
loic.michalon <loic.michalon@ekium.eu>
2023-10-20 09:39:31 +0200

    [EDIT] Migrations

---
+++ Core/Migrations/20231020072639_DetectionColumnsRename.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    public partial class DetectionColumnsRename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TSShooting",
+                table: "Packet",
+                newName: "ShootingTS");
+
+            migrationBuilder.RenameColumn(
+                name: "IsMismatched",
+                table: "Packet",
+                newName: "IsSameType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShootingTS",
+                table: "Packet",
+                newName: "TSShooting");
+
+            migrationBuilder.RenameColumn(
+                name: "IsSameType",
+                table: "Packet",
+                newName: "IsMismatched");
+        }
+    }
+}
