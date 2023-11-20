411a3adab4c59c4e577739f0fc7fd8ab9eb79644
loic.michalon <loic.michalon@ekium.eu>
2023-11-20 16:43:17 +0100

    [FEAT] Migrations

---
+++ Core/Migrations/20231120130521_addIndexBenchmarkRIDAndDotNet8.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIndexBenchmarkRIDAndDotNet8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "StationCycle",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Packet",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "IOTTag",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "IOTDevice",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RID",
+                table: "BenchmarkTest",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Anode",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BenchmarkTest_RID",
+                table: "BenchmarkTest",
+                column: "RID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BenchmarkTest_RID",
+                table: "BenchmarkTest");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "StationCycle",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(13)",
+                oldMaxLength: 13);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Packet",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "IOTTag",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(13)",
+                oldMaxLength: 13);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "IOTDevice",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(13)",
+                oldMaxLength: 13);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RID",
+                table: "BenchmarkTest",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Anode",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(8)",
+                oldMaxLength: 8);
+        }
+    }
+}
---
+++ Core/Migrations/20231120145540_CameraTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class CameraTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CameraTest",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TS = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CameraTest", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BenchmarkTest_CameraID",
+                table: "BenchmarkTest",
+                column: "CameraID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BenchmarkTest_CameraTest_CameraID",
+                table: "BenchmarkTest",
+                column: "CameraID",
+                principalTable: "CameraTest",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BenchmarkTest_CameraTest_CameraID",
+                table: "BenchmarkTest");
+
+            migrationBuilder.DropTable(
+                name: "CameraTest");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BenchmarkTest_CameraID",
+                table: "BenchmarkTest");
+        }
+    }
+}
