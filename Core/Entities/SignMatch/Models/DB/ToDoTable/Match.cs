779bb02cbcf5872708dbf5cdce66a9657e526977
Lowen Desmarais <lowen.desmarais@ekium.eu>
2024-03-12 09:16:12 +0100

    added models for sign and match

---
+++ Core/Entities/SignMatch/Models/DB/ToDoTable/Match.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.StationCycles.Models.DB.MatchableCycles;
+using Core.Shared.Models.DB.Kernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.SignMatch.Models.DB.ToDoTable
+{
+    public class Match : SignedCycle
+    {
+        public int MatchableCycleId { get; set; }
+    }
+}
---
+++ Core/Entities/SignMatch/Models/DB/ToDoTable/Sign.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.SignMatch.Models.DB.ToDoTable
+{
+    public class Sign
+    {
+        public int CycleID { get; set; }
+        public int CycleRID { get; set; }
+        public int CamID { get; set; }
+        public int StationID { get; set; }
+        public DateTimeOffset? CycleTS { get; set; }
+    }
+}
