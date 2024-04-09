300cdf23451fe8861c2abc0ac785e552ba064ebb
Lowen Desmarais <lowen.desmarais@ekium.eu>
2024-04-09 10:31:12 +0200

    added oldestTSShooting and IsOptional

--- Core/Entities/IOT/IOTDevices/Models/DB/ITApis/ITApiStations/ITApiStation.cs
+++ Core/Entities/IOT/IOTDevices/Models/DB/ITApis/ITApiStations/ITApiStation.cs
@@ -7,4 +7,5 @@ namespace Core.Entities.IOT.IOTDevices.Models.DB.ITApiStations;
 public partial class ITApiStation : ITApi, IBaseEntity<ITApiStation, DTOITApiStation>
 {
 	public DateTimeOffset OldestTSShooting { get; set; }
+	public bool IsOptional { get; set; }
 }
--- Core/Entities/IOT/IOTDevices/Models/DB/ITApis/ITApiStations/ITApiStationMethod.cs
+++ Core/Entities/IOT/IOTDevices/Models/DB/ITApis/ITApiStations/ITApiStationMethod.cs
@@ -1,5 +1,7 @@
 using System.Net;
+using System.Text.Json;
 using Core.Entities.IOT.IOTDevices.Models.DTO.ITApiStations;
+using Core.Shared.Models.ApiResponses;
 using Microsoft.Extensions.Logging;
 
 namespace Core.Entities.IOT.IOTDevices.Models.DB.ITApiStations;
@@ -24,15 +26,34 @@ public partial class ITApiStation
 		using HttpClient httpClient = new();
 		CancellationTokenSource cancelSource = new();
 		bool isConnected;
-		try
+
+		if (IsOptional)
 		{
-			HttpResponseMessage response = await httpClient.GetAsync(Address + ConnectionPath);
-			isConnected = response.StatusCode == HttpStatusCode.OK;
+			isConnected = true;
+			OldestTSShooting = DateTimeOffset.Now;
 		}
-		catch (Exception e)
+		else
 		{
-			logger.LogInformation("Error while trying to connect to {name}:\n{error}", RID, e);
-			isConnected = false;
+			try
+			{
+				HttpResponseMessage response = await httpClient.GetAsync(Address + ConnectionPath);
+				isConnected = response.StatusCode == HttpStatusCode.OK;
+
+				ApiResponse? apiResponse
+				= JsonSerializer.Deserialize<ApiResponse>(await response.Content.ReadAsStreamAsync());
+				if (apiResponse is null)
+					throw new ApplicationException("Could not deserialize ApiIOT response");
+
+				if (apiResponse.Result is not JsonElement jsonElement)
+					throw new ApplicationException("JSON Exception, ApiResponse from ApiIOT is broken");
+
+				OldestTSShooting = jsonElement.Deserialize<DateTimeOffset>();
+			}
+			catch (Exception e)
+			{
+				logger.LogInformation("Error while trying to connect to {name}:\n{error}", RID, e);
+				isConnected = false;
+			}
 		}
 
 		return isConnected;
