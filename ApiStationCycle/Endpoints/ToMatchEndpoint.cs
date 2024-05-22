59f2facd9e8ae13e7a32b081a1b7e2e6060482b1
Nemuel <nemuel.pageleonie@ekium.eu>
2024-05-22 10:09:41 +0200

    feat: add ToMatch and ToSign endpoint

---
+++ ApiStationCycle/Endpoints/ToMatchEndpoint.cs
@@ -0,0 +1,21 @@
+using Carter;
+using Core.Entities.Vision.ToDos.Models.DB.ToMatchs;
+using Core.Entities.Vision.ToDos.Models.DTO.ToMatchs;
+using Core.Entities.Vision.ToDos.Services.ToMatchs;
+using Core.Shared.Dictionaries;
+using Core.Shared.Endpoints.Kernel;
+using Core.Shared.Endpoints.Kernel.Dictionaries;
+
+namespace ApiStationCycle.Endpoints;
+
+public class ToMatchEndpoint : BaseEntityEndpoint<ToMatch, DTOToMatch, IToMatchService>, ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		if (Station.IsServer)
+			return;
+
+		RouteGroupBuilder group = app.MapGroup("apiStationCycle").WithTags(nameof(ToMatchEndpoint));
+		group = MapBaseEndpoints(group, BaseEndpointFlags.Read);
+	}
+}
